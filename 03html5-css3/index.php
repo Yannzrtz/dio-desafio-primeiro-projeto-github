18dbf735c620098752bb2748de083ab242eb52a4
Yannzrtz <zurutuza.yann@gmail.com>
2022-06-26 17:48:10 -0300

    adiciona HTML5 e CSS3

---
+++ 03html5-css3/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Yann Zurutuza</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<img src="a1.PNG"alt="aleatória" class="photo"> 
+		<h1 id="title">Yann Zurutuza</h1>
+	</header>
+	<section>
+		<header>
+			<h2 class="subtitle">Posts</h2>
+		</header>
+		<article class="post">
+			<header>
+				<h3 class="post_title"> Post #1</h3>
+				<img src="a2.PNG" alt="aleatória2" class="post_image">
+			</header>
+			<p class="post_content">
+				Lorem ipsum dolor sit amet, <a href="https://github.com/Yannzrtz"target="_blank">consectetur adipiscing elit</a>. Duis et erat at tortor placerat venenatis. Integer eu feugiat velit. Sed feugiat pretium elit, et tincidunt metus fermentum et. Vestibulum convallis sagittis est, <a href="mailto:zurutuza.yann@gmail.com">vel feugiat tortor posuere quis</a>. Quisque suscipit lobortis turpis eu bibendum. Vestibulum placerat, ante ut scelerisque convallis, quam enim vulputate nibh, sed posuere lacus sapien at lorem. Quisque condimentum sagittis purus, vel pellentesque quam gravida vel. Integer commodo lacinia vulputate. Sed vel turpis sed odio dapibus gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+			</p>
+		</article>
+	</section>
+	<footer>
+		<ul class="contacts_list">
+			<li>
+				<a href="mailto:zurutuza.yann@gmail.com">zurutuza.yann@gmail.com</a>
+			</li>
+			<li>
+				<a href="https://github.com/Yannzrtz" target="_blank">GitHub</a>
+			</li>
+		</ul>
+	</footer>
+</body>
+</html>
